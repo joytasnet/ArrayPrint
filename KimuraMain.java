6cf058414e78b55f3126bfda4aea8edb13a44458
wadaka <erou1227@yahoo.co.jp>
2022-04-05 13:15:15 +0900

    記述短縮版

--- KimuraMain.java
+++ KimuraMain.java
@@ -5,14 +5,10 @@ public class KimuraMain{
 		nums[0] = new int[]{2,3,4};
 		nums[1] = new int[]{4,1,4,5};
 		nums[2] = new int[]{8,3};
+		System.out.println("{");
 		for(int i=0;i<nums.length;i++){
-			if(i==0){
-				System.out.println("{");
-			}
+			System.out.printf("\t{");
 			for(int j=0;j<nums[i].length;j++){
-				if(j==0){
-					System.out.printf("\t{");
-				}
 				System.out.print(nums[i][j]);
 				if(j==nums[i].length-1){
 					break;
