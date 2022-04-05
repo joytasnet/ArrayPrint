a6f86a36b934be3abed9a84678d642f0ac5e0d32
yakan <yume_drop@hotmail.com>
2022-04-05 13:15:10 +0900

    Array追加

---
+++ NaganoMain.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class NaganoMain{
+	public static void main(String[] args){
+		int[][] nums={
+			{2,3,4},
+			{4,1,4,5},
+			{8,3},
+		};
+		System.out.println("{");
+		for(int i =0;i<nums.length;i++){
+			System.out.printf("\t{");
+			for(int j = 0;j<nums[i].length;j++){
+				System.out.print(nums[i][j]+",");
+			}
+			System.out.println("},");
+		}
+		System.out.println("}");
+	}
+}
